fdc3db9b869097466dbbbb62e35fe6ebef7d1705
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-06 11:35:33 +0200

    Add Builder annotations to Course and Teacher classes
    The Builder annotation from the Lombok library has been added to both the Course and Teacher classes. This allows for easier and more concise object instantiation, particularly when dealing with objects that have many parameters.

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,10 +1,12 @@
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class Course {
     long id;
     String name;
--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,4 +1,7 @@
+import lombok.Builder;
+
 import java.util.Set;
 
+@Builder
 public record Teacher(long id, String name, Set<String> subjects) {
 }
