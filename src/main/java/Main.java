00bd49424a5a570ffb4e7117c1d6d0c66fd92e52
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-06 11:16:16 +0200

    Add new Student instances and update Course
    The main function in Main.java is now creating more instances of the Student class. Besides updating the Student class with the Builder pattern, it modifies the Course instance to include these new students. This changes the persons available in the Java Bootcamp course, which now includes Aljoscha, Jonas, Gökhan, and Daniel.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,29 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         Student aljoscha = new Student(1, "Aljoscha", "Berlin", 1.0F);
+        Student jonas = new Student();
+        Student goekhan = new Student();
+        Student daniel = Student.builder().build();
         System.out.println(aljoscha);
+
+        jonas.setId(2);
+        jonas.setName("Jonas");
+
+        goekhan.builder()
+                .id(3)
+                .name("Gökhan")
+                .build();
+
+        Student daniel1 = daniel.builder().name("Daniel").id(4).build();
+
+        System.out.println("daniel1: " + daniel1);
+
+        System.out.println("Daniel: " + daniel);
 
         Teacher florian = new Teacher(1, "Florian", Set.of("Java", "Web") );
         System.out.println(florian);
 
-        Course javaBootcamp = new Course(1, "Java Bootcamp", florian, Set.of(aljoscha));
+        Course javaBootcamp = new Course(1, "Java Bootcamp", florian, Set.of(aljoscha, jonas, goekhan,daniel1));
         System.out.println(javaBootcamp);
     }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,10 +1,12 @@
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Student {
     private long id;
     private String name;
